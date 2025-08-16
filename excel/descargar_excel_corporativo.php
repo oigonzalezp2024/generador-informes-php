29f89a3936e2e14b44850a0a27759d6c221661f9
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2025-08-16 17:19:32 -0500

    feat(config): módulo estable facilmente configurable

--- excel/descargar_excel_corporativo.php
+++ excel/descargar_excel_corporativo.php
@@ -1,16 +1,25 @@
 <?php
+// Carga el autoloader de Composer para que las clases sean reconocidas.
 require '../vendor/autoload.php';
 
 use App\Infrastructure\DescargarExcelCorporativo;
-use App\Application\GenerarInformeGestion;
-use App\Infrastructure\Repository\InformeGestionExcelRepository;
+use App\Application\BodegaGenerarInformeGestion;
+use App\Infrastructure\Repository\BodegaInformeGestionRepository;
 
-// Ruta al archivo de configuración.
-$configPath = './config.php';
+// Define la ruta al archivo de configuración.
+$configPath = '../config.php';
 
-$informeGestionExcelRepository = new InformeGestionExcelRepository();
+// Carga el archivo de configuración y captura el array que retorna.
+// Se usa "require" para ejecutar el archivo y obtener su valor de retorno.
+$config = require $configPath;
+
+// Ahora que $config es un array, se puede pasar al constructor.
+$data = $config['data'];
+
+$informeGestionExcelRepository = new BodegaInformeGestionRepository();
+$informeGestionExcelRepository->setData($data);
 $datosSimulacion = $informeGestionExcelRepository->getData();
-$generarInformeGestion = new GenerarInformeGestion();
+$generarInformeGestion = new BodegaGenerarInformeGestion($config);
 $registros = $generarInformeGestion->run($datosSimulacion);
 $excelGenerator = new DescargarExcelCorporativo($configPath, $registros);
 $excelGenerator->descargar();
